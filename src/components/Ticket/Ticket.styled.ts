b14058aac94a29a3ff0650c4404a68e0870ff7c2
ezgi.a <ezgi.a@affinidi.com>
2023-01-25 09:50:53 +0100

    feat: first draft of ticket component

--- src/components/Ticket/Ticket.styled.ts
+++ src/components/Ticket/Ticket.styled.ts
@@ -21,7 +21,6 @@ export const TicketWrapper = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
-  // border-radius: 15px;
 `
 
 export const TicketHeader = styled.div<{ $isActive: boolean }>`
