aa49bc66157b8f34f175b044431b505cb14c70d9
Ansha Kommu <anusha.k@affinidi.com>
2023-01-26 12:16:35 +0100

    feat(FTL-8795): remove commented code

--- src/modules/verifier/pages/Welcome/Welcome.styled.tsx
+++ src/modules/verifier/pages/Welcome/Welcome.styled.tsx
@@ -4,11 +4,6 @@ import { pxToRem } from 'utils'
 
 export const IconContainer = styled.div`
   margin: ${pxToRem(44)} 0 ${pxToRem(48)};
-
-  // @media (min-width: 500px) {
-  //   margin-left: 0;
-  //   margin-right: 0;
-  // }
 `
 
 export const CenterDiv = styled.div`
