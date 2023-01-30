be5a89f83d4328ac53f10796976c407c177708cd
anusha-kommu <107613773+anusha-kommu@users.noreply.github.com>
2023-01-30 11:27:28 +0100

    feat(FTL-8805): update event page with start date and end date (#17)
    * feat(FTL-8805): update design
    
    * feat(FTL-8805): update schema url and credential subject
    
    * feat(FTL-8805): refactor code

--- src/modules/issuer/CredentialForm/useCredentialForm.ts
+++ src/modules/issuer/CredentialForm/useCredentialForm.ts
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router'
-import { format, isMatch } from 'date-fns'
+import { format } from 'date-fns'
 import { useCallback } from 'react'
 import * as EmailValidator from 'email-validator'
 
@@ -17,8 +17,8 @@ import { adjustForUTCOffset } from './CredentialForm'
 export type EventSubjectData = {
   eventName: string
   eventLocation: string
-  eventDate: string
-  eventTime: string
+  eventStartDateTime: string
+  eventEndDateTime: string
   eventDescription: string
   name: string
   email: string
@@ -27,14 +27,14 @@ export type EventSubjectData = {
 export const initialValues: EventSubjectData = {
   eventName: '',
   eventLocation: '',
-  eventDate: '',
-  eventTime: '',
+  eventStartDateTime: '',
+  eventEndDateTime: '',
   eventDescription: '',
   name: '',
   email: '',
 }
 
-export const schema = 'https://schema.affinidi.com/EventElegibilityV1-0.json'
+export const schema = 'https://schema.affinidi.com/EventEligibilityV1-0.json'
 
 export const useCredentialForm = () => {
   const navigate = useNavigate()
@@ -71,8 +71,14 @@ export const useCredentialForm = () => {
           },
         },
         credentialSubject: {
-          date: format(adjustForUTCOffset(new Date(values.eventDate)), "yyyy-MM-dd'T'HH:mm:ss'Z'"),
-          time: values.eventTime,
+          startDate: format(
+            adjustForUTCOffset(new Date(values.eventStartDateTime)),
+            "yyyy-MM-dd'T'HH:mm:ss'Z'",
+          ),
+          endDate: format(
+            adjustForUTCOffset(new Date(values.eventEndDateTime)),
+            "yyyy-MM-dd'T'HH:mm:ss'Z'",
+          ),
           place: values.eventLocation,
           eventName: values.eventName,
           eventDescription: values.eventDescription,
@@ -96,14 +102,20 @@ export const useCredentialForm = () => {
       errors.eventName = 'Mandatory field'
     }
 
-    if (!values.eventDate) {
-      errors.eventDate = 'Mandatory field'
-    } else if (!isMatch(values.eventDate, 'dd/MM/yyyy')) {
-      errors.eventDate = 'Please enter a valid date'
+    if (!values.eventStartDateTime) {
+      errors.eventStartDateTime = 'Mandatory field'
     }
 
-    if (!values.eventTime) {
-      errors.eventTime = 'Mandatory field'
+    if (!values.eventEndDateTime) {
+      errors.eventEndDateTime = 'Mandatory field'
+    }
+
+    if (
+      values.eventStartDateTime &&
+      values.eventEndDateTime &&
+      new Date(values.eventStartDateTime) > new Date(values.eventEndDateTime)
+    ) {
+      errors.eventStartDateTime = 'Start date time must not be greater than end date time'
     }
 
     if (!values.eventLocation) {
@@ -132,3 +144,4 @@ export const useCredentialForm = () => {
     validate,
   }
 }
+
