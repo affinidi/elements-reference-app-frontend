4c0c33fbe53b8af730123843b532b06893594083
anusha-kommu <107613773+anusha-kommu@users.noreply.github.com>
2023-01-30 17:38:02 +0100

    feat(FTL-8805): implement ticket details component (#19)
    * feat: implement ticket details component
    
    * feat: refactor code
    
    * feat: fixed comments
    
    ---------
    
    Co-authored-by: ezgi.a <ezgi.a@affinidi.com>

---
+++ src/components/TicketDetails/TicketDetails.styled.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+import { pxToRem } from 'utils'
+import { Typography } from 'components'
+
+export const TicketDetailsCardLeft = styled.div`
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  width: ${pxToRem(840)};
+  height: ${pxToRem(372)};
+  padding: ${pxToRem(40)};
+  background-color: ${(props) => props.theme.colors.utility.info['100']};
+`
+
+export const TicketDetailsCardRight = styled.div`
+  background-color: ${(props) => props.theme.colors.utility.info['100']};
+  position: relative;
+  border-left: 0.18em dashed #fff;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  height: ${pxToRem(372)};
+  padding: ${pxToRem(56)};
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    display: block;
+    width: 1em;
+    height: 1em;
+    background: ${(props) => props.theme.colors.neutral.secondary['100']};
+    border-radius: 50%;
+    left: ${pxToRem(-9)};
+  }
+  &::before {
+    top: ${pxToRem(-8)};
+  }
+  &::after {
+    bottom: ${pxToRem(-8)};
+  }
+`
+
+export const Data = styled(Typography)`
+  color: ${(props) => props.theme.colors.neutral.secondary['100']};
+`
